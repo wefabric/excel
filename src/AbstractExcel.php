63e035533aa517cb465db464ff41a7a1d5fcf40a
Leo Flapper <dominicus1991@gmail.com>
2022-01-27 14:09:16 +0100

    Added currency formatting

--- src/AbstractExcel.php
+++ src/AbstractExcel.php
@@ -4,10 +4,17 @@
 namespace Wefabric\Excel;
 
 use Illuminate\Database\Eloquent\Model;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 
-class AbstractExcel implements Excellable
+class AbstractExcel implements Excellable, WithColumnFormatting
 {
+
+    CONST EUR_CURRENCY_FORMAT = '_("€"* #,##0.00_)';
+    CONST EUR_CURRENCY_FORMAT_PRECISE = '_("€"* #,##0.0000_)';
+
     public static array $headings;
+    public array $columnFormats = [];
+
     public Model $model;
 
     public function __construct(Model $model)
@@ -19,6 +26,12 @@ class AbstractExcel implements Excellable
     {
         return $this->model->toArray();
     }
+
+    public function columnFormats(): array
+    {
+        return $this->columnFormats;
+    }
+
 
     /**
      * @param Model $model
